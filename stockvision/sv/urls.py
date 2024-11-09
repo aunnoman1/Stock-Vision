b772c8ef966afc4c914d6388c0311693c287716b
saqibali1059 <doctorx1047@gmail.com>
2024-11-10 00:19:07 +0500

    a

--- stockvision/sv/urls.py
+++ stockvision/sv/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.homepage_view, name='home'),
     path('stock/<str:symbol>/', views.stock_detail, name='stock_detail'),
-    path('search/', views.search_stock, name='search_stock'),
+    path('search_stocks/', views.search_stocks, name='search_stocks'),
 ]
--- stockvision/sv/views.py
+++ stockvision/sv/views.py
@@ -1,43 +1,30 @@
 from django.shortcuts import render
-from .models import Price,Stock
-
+from django.shortcuts import  get_object_or_404
+from .models import Stock
 def homepage_view(request):
     return render(request, 'home.html')
 
+
 def stock_detail(request, symbol):
-    # Retrieve stock data from the database
-    stock_data = get_object_or_404(Stock, ticker=symbol)
-    
-    # Retrieve all historical prices for this stock
-    stock_prices = Price.objects.filter(stock=stock_data).order_by('-day')
-    
-    # Get the most recent closing price as the current price if it exists
-    current_price = stock_prices.first().close_price if stock_prices.exists() else None
-    
-    context = {
-        'stock_data': stock_data,
-        'stock_prices': stock_prices,
-        'current_price': current_price,
-    }
-    
-    return render(request, 'stock_detail.html', context)
 
-def search_stock(request):
-    query = request.GET.get('query')
+    stock = get_object_or_404(Stock, ticker=symbol)
+    
+
+    latest_price = stock.prices.order_by('-day').first()
+
+    return render(request, 'onestockpage.html', {
+        'stock': stock,
+        'latest_price': latest_price
+    })
+
+from django.http import JsonResponse
+
+def search_stocks(request):
+    query = request.GET.get('q', '')
     if query:
-        # Filter stocks by name or ticker symbol containing the search query
-        search_results = Stock.objects.filter(name__icontains=query) | Stock.objects.filter(ticker__icontains=query)
-
-        # Attach latest price data for each stock
-        for stock in search_results:
-            latest_price = stock.prices.order_by('-day').first()
-            stock.latest_price = latest_price  # Attach entire Price object if it exists
+        stocks = Stock.objects.filter(name__icontains=query)[:10]
+        results = [{'name': stock.name, 'ticker': stock.ticker} for stock in stocks]
     else:
-        search_results = None
+        results = []
     
-    context = {
-        'query': query,
-        'search_results': search_results,
-    }
-    
-    return render(request, 'search_results.html', context)
+    return JsonResponse(results, safe=False)
