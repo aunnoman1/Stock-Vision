a677f4b640df195d115e8fc3723cf3818ad4e389
saqibali1059 <doctorx1047@gmail.com>
2024-11-19 21:45:19 +0500

    watchlist add & remove

--- stockvision/sv/urls.py
+++ stockvision/sv/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('add_to_watchlist/<int:stock_id>/', views.add_to_watchlist, name='add_to_watchlist'),
+    path('remove_from_watchlist/<int:stock_id>/', views.remove_from_watchlist, name='remove_from_watchlist'),
+    path('watchlist/remove/<int:stock_id>/', views.remove_from_watchlist, name='remove_from_watchlist'),
+    path('watchlist/stock/<str:symbol>/', views.stock_detail, name='stock_detail'),
 ]
